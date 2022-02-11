87b3210771d4e5a57efac5b1fbfd736b6f729e55
Matheus Lopes Marques <2000ttu@gmail.com>
2022-02-11 15:28:28 -0300

    [Improvement] Improve keyboard commands

--- main.js
+++ main.js
@@ -1,40 +1,67 @@
-const { app, BrowserWindow, Menu, ipcMain } = require('electron')
+const {
+  app,
+  BrowserWindow,
+  globalShortcut,
+  ipcRenderer,
+  ipcMain,
+  remote,
+} = require("electron");
 var mainWindow;
+let data = {
+  width: 600,
+  height: 400,
+  x: 3100,
+  y: 90,
+};
+
 const createWindow = () => {
-    mainWindow = new BrowserWindow({
-    width: 600,
-    height: 750,
+  mainWindow = new BrowserWindow({
+    width: data.width,
+    height: data.height,
     alwaysOnTop: true,
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false
-    }
-  })
+      contextIsolation: false,
+    },
+  });
 
   mainWindow.webContents.openDevTools(true);
-  mainWindow.setPosition(-650, 200)
-  mainWindow.loadFile('./index.html')
-  
-}
+  mainWindow.setPosition(data.x, data.y);
+  mainWindow.loadFile("./index.html");
+};
 
 app.whenReady().then(() => {
-  createWindow()
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
-ipcMain.on('getData', (event) => {
-  let currentPos = mainWindow.getPosition()
-  let currentSize = mainWindow.getSize()
-  let data = {
-    width: currentSize[0],
-    height: currentSize[1],
-    x: currentPos[0],
-    y: currentPos[1]
-  }
-  event.reply('sendData', data);
-})
-ipcMain.on('changeData', (event, data) => {
-  mainWindow.setSize(data.width, data.height)
-  mainWindow.setPosition(data.x, data.y)
-})
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+  globalShortcut.register("up", () => {
+    data.y = mainWindow.getPosition[1] - 1;
+    updateWindow(data);
+    mainWindow.webContents.send("sendData", data);
+  });
+  globalShortcut.register("down", () => {
+    data.y = mainWindow.getPosition[1] + 1;
+    updateWindow(data);
+    mainWindow.webContents.send("sendData", data);
+  });
+  globalShortcut.register("left", () => {
+    data.x = mainWindow.getPosition[0] - 1;
+    updateWindow(data);
+    mainWindow.webContents.send("sendData", data);
+  });
+  globalShortcut.register("right", () => {
+    data.x = mainWindow.getPosition[0] + 1;
+    updateWindow(data);
+    mainWindow.webContents.send("sendData", data);
+  });
+});
+
+function updateWindow(data) {
+  mainWindow.setSize(data.width, data.height);
+  mainWindow.setPosition(data.x, data.y);
+}
+
+app.on("move", (event) => {
+  console.log(event);
+});
--- script.js
+++ script.js
@@ -1,31 +1,15 @@
-const { ipcRenderer, ipcMain } = require('electron')
+const { ipcRenderer } = require("electron");
 
 let frame = {
   width: 0,
   height: 0,
   x: 0,
   y: 0,
-}
-let ruler = document.getElementById('ruler')
+};
 
-window.addEventListener('keydown', (e) => {
-  ipcRenderer.send('getData');
-  ipcRenderer.on('sendData', (event, data) => {
-    frame = data
-         if (e.code === 'ArrowDown') {frame.y += 1}
-    else if (e.code === 'ArrowUp') {frame.y -= 1}
-    else if (e.code === 'ArrowRight') {frame.x += 1}
-    else if (e.code === 'ArrowLeft ') {frame.x -= 1}
-    ipcRenderer.send('changeData', frame);
-  })
-})
+const ruler = document.getElementById("ruler");
 
-window.addEventListener('wheel', (e) => {
-  ipcRenderer.send('getData');
-  ipcRenderer.on('sendData', (event, data) => {
-    var dir = Math.sign(e.deltaY);
-    data.width -= dir
-
-    ipcRenderer.send('changeData', data);
-  })
-})
+ipcRenderer.on("sendData", (event, data) => {
+  frame = data;
+  console.log(frame);
+});
