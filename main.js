a8fdf200a21c3e5043b4cf48778701f7280a301f
Matheus Lopes Marques <2000ttu@gmail.com>
2022-02-11 00:00:13 -0300

    [Improvement] wheel and keydown events suported, but with flicks

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const { app, BrowserWindow, Menu, ipcMain } = require('electron')
-
+var mainWindow;
 const createWindow = () => {
-  const mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
     width: 600,
-    height: 150,
+    height: 750,
     alwaysOnTop: true,
     webPreferences: {
       nodeIntegration: true,
@@ -11,27 +11,30 @@ const createWindow = () => {
     }
   })
 
-  /* mainWindow.webContents.openDevTools(true); */
-  mainWindow.setPosition(-700, 200)
+  mainWindow.webContents.openDevTools(true);
+  mainWindow.setPosition(-650, 200)
   mainWindow.loadFile('./index.html')
-  ipcMain.on('ping', (event, data) => {
-    let currentPos = mainWindow.getPosition()
-    let currentSize = mainWindow.getSize()
-    mainWindow.setSize(data.width, data.height)
-    mainWindow.setPosition(parseInt(currentPos[0] - data.x), parseInt(currentPos[1] - data.y))
-    let newData = {
-      width: data.width,
-      height: data.height,
-      x: data.x,
-      y: data.y
-    }
-  })
+  
 }
 
 app.whenReady().then(() => {
   createWindow()
-
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
+})
+ipcMain.on('getData', (event) => {
+  let currentPos = mainWindow.getPosition()
+  let currentSize = mainWindow.getSize()
+  let data = {
+    width: currentSize[0],
+    height: currentSize[1],
+    x: currentPos[0],
+    y: currentPos[1]
+  }
+  event.reply('sendData', data);
+})
+ipcMain.on('changeData', (event, data) => {
+  mainWindow.setSize(data.width, data.height)
+  mainWindow.setPosition(data.x, data.y)
 })
--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 const { ipcRenderer, ipcMain } = require('electron')
 
-let windowPosition = {}
-let x = 0;
-let y = 0;
-let multiplier = 1;
+let frame = {
+  width: 0,
+  height: 0,
+  x: 0,
+  y: 0,
+}
 let ruler = document.getElementById('ruler')
 
 window.addEventListener('keydown', (e) => {
-  if (e.code === 'ShiftLeft') multiplier = 5
-  if (e.code === 'ArrowDown')  y = 1*multiplier 
-  if (e.code === 'ArrowUp')  y = -1*multiplier 
-  if (e.code === 'ArrowRight')  x = 1*multiplier 
-  if (e.code === 'ArrowLeft ')  x = -1*multiplier 
-  let data = {
-    width: 450,
-    height: 600,
-    x: x,
-    y: y,
-  }
-  ipcRenderer.send('ping', data)
+  ipcRenderer.send('getData');
+  ipcRenderer.on('sendData', (event, data) => {
+    frame = data
+         if (e.code === 'ArrowDown') {frame.y += 1}
+    else if (e.code === 'ArrowUp') {frame.y -= 1}
+    else if (e.code === 'ArrowRight') {frame.x += 1}
+    else if (e.code === 'ArrowLeft ') {frame.x -= 1}
+    ipcRenderer.send('changeData', frame);
+  })
 })
 
+window.addEventListener('wheel', (e) => {
+  ipcRenderer.send('getData');
+  ipcRenderer.on('sendData', (event, data) => {
+    var dir = Math.sign(e.deltaY);
+    data.width -= dir
 
-ipcRenderer.on('pong', (event, data) => {
-  windowPosition = data
-  console.log(windowPosition);
+    ipcRenderer.send('changeData', data);
+  })
 })
