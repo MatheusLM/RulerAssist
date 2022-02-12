e5b5b2429adfc4259056b93eed5a019ea3d6071d
Matheus Lopes Marques <2000ttu@gmail.com>
2022-02-12 13:04:01 -0300

    [Add] Added symmetrical ruler with metrics, and added resize event for change values

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { app, BrowserWindow, globalShortcut } = require("electron");
 var mainWindow;
 let data = {
   width: 600,
-  height: 60,
+  height: 56,
   x: 300,
   y: 970
 };
@@ -17,11 +17,16 @@ let theme = {
 let controls = {
   fast: false,
   modifier: 1,
-  centered: true,
+  symmetrical: false,
   markers: 3,
+  showSizes: false,
   kiosk: false
 }
 
+function updateWindow() {
+  mainWindow.setSize(data.width, data.height);
+  mainWindow.setPosition(data.x, data.y);
+}
 function updateData() {
   let pos = mainWindow.getPosition()
   let size = mainWindow.getSize()
@@ -42,11 +47,9 @@ const createWindow = () => {
       nodeIntegration: true,
       contextIsolation: false,
     },
-    width: data.width,
-    height: data.height,
     opacity: theme.opacity,
-    minWidth: 60,
-    minHeight: 60,
+    minWidth: 48,
+    minHeight: 56,
     maxWidth: 1920,
     maxHeight: 1080,
     alwaysOnTop: true,
@@ -61,99 +64,111 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow();
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+
+  mainWindow.on('resize', () => {
+    updateData()
+    mainWindow.webContents.send("sendControls", controls, data);
   });
+
+  updateData()
   setupKeys(globalShortcut);
 });
-
-function updateWindow() {
-  mainWindow.setSize(data.width, data.height);
-  mainWindow.setPosition(data.x, data.y);
-}
 
 app.on("moved", (e) => {
   console.log(e);
 })
 
+function sendControls(controls, data){
+  mainWindow.webContents.send("sendControls", controls, data);
+}
+
 function setupKeys(globalShortcut){
   // window data
-  globalShortcut.register("CmdOrCtrl+up", () => {
+  globalShortcut.register("Alt+up", () => {
     updateData();
     data.y -= controls.modifier;
     updateWindow()
   });
-  globalShortcut.register("CmdOrCtrl+down", () => {
+  globalShortcut.register("Alt+down", () => {
     updateData();
     data.y += controls.modifier;
     updateWindow()
   });
-  globalShortcut.register("CmdOrCtrl+left", () => {
+  globalShortcut.register("Alt+left", () => {
     updateData();
     data.x -= controls.modifier;
     updateWindow()
   });
-  globalShortcut.register("CmdOrCtrl+right", () => {
+  globalShortcut.register("Alt+right", () => {
     updateData();
     data.x += controls.modifier;
     updateWindow()
   });
 
   // theme data
-  globalShortcut.register("Alt+num1", () => {
-    theme.flipped = !theme.flipped;
-    let size = mainWindow.getSize()
-    data.width = size[1];
-    data.height = size[0];
-    mainWindow.webContents.send("sendTheme", theme);
-    updateWindow()
-  });
-  globalShortcut.register("Alt+num0", () => {
+  globalShortcut.register("Alt+T", () => {
+    updateData()
     theme.dark = !theme.dark;
     mainWindow.webContents.send("sendTheme", theme);
     updateWindow()
   });
-  globalShortcut.register("Alt+numadd", () => {
+  globalShortcut.register("Alt+F", () => {
+    updateData()
+    theme.flipped = !theme.flipped;
+    /* let size = mainWindow.getSize()
+    data.width = size[1];
+    data.height = size[0]; */
+    mainWindow.webContents.send("sendTheme", theme);
+    updateWindow()
+  });
+  globalShortcut.register("Alt+PageUp", () => {
     theme.flipped = !theme.flipped;
     theme.opacity += (theme.opacity < 1) ? 0.05 : 0;
     updateOpacity()
   });
-  globalShortcut.register("Alt+numsub", () => {
+  globalShortcut.register("Alt+PageDown", () => {
     theme.flipped = !theme.flipped;
     theme.opacity -= (theme.opacity > 0.1) ? 0.05 : 0;
     updateOpacity()
   });
 
   // controls data
+  // fast mode
   globalShortcut.register("Scrolllock", () => {
     controls.fast = !controls.fast;
     controls.modifier = (controls.fast)? 10 : 1;
   });
+  // Size controls
   globalShortcut.register("CmdOrCtrl+numadd", () => {
+    updateData()
     data.width += controls.modifier;
     updateWindow()
-    updateData()
-    mainWindow.webContents.send("sendControls", controls, data);
+    sendControls(controls, data)
   });
   globalShortcut.register("CmdOrCtrl+numsub", () => {
+    updateData()
     data.width -= controls.modifier;
     updateWindow()
-    updateData()
-    mainWindow.webContents.send("sendControls", controls, data);
+    sendControls(controls, data)
   });
-  globalShortcut.register("CmdOrCtrl+Shift+numadd", () => {
-    controls.markers += 1;
+  // Markers controls
+  globalShortcut.register("Alt+Insert", () => {
+    updateData()
+    let step = (controls.symmetrical) ? 2 : 1
+    controls.markers += (controls.markers < 30) ? step : 0
+    sendControls(controls, data)
     updateWindow()
-    updateData()
-    mainWindow.webContents.send("sendControls", controls, data);
   });
-  globalShortcut.register("CmdOrCtrl+Shift+numsub", () => {
-    controls.markers += 1;
+  globalShortcut.register("Alt+Delete", () => {
+    updateData()
+    let step = (controls.symmetrical) ? 2 : 1
+    controls.markers -= (controls.markers > 2) ? step : 0
+    sendControls(controls, data)
     updateWindow()
-    updateData()
-    mainWindow.webContents.send("sendControls", controls, data);
   });
-  globalShortcut.register("CmdOrCtrl+Shift+K", () => {
+  // Kiosk controls
+  globalShortcut.register("Alt+K", () => {
+    updateData()
     controls.kiosk = !controls.kiosk
     mainWindow.kiosk = controls.kiosk
     if(controls.kiosk){
@@ -161,15 +176,24 @@ function setupKeys(globalShortcut){
     }else{
       mainWindow.setIgnoreMouseEvents(false)
     }
+    sendControls(controls, data)
   });
+  // 
   globalShortcut.register("Shift+numadd", () => {
-    controls.markers += (controls.markers < 30) ? 1 : 0
-    updateData()
-    mainWindow.webContents.send("sendControls", controls, data);
+
   });
   globalShortcut.register("Shift+numsub", () => {
-    controls.markers -= (controls.markers > 1) ? 1 : 0
+    
+  });
+  // Symmetrical ruler
+  globalShortcut.register("Alt+num0", () => {
     updateData()
-    mainWindow.webContents.send("sendControls", controls, data);
+    controls.symmetrical = !controls.symmetrical
+    sendControls(controls, data)
+  });
+  globalShortcut.register("Alt+Enter", () => {
+    updateData()
+    controls.showSizes = !controls.showSizes
+    sendControls(controls, data)
   });
 }
--- script.js
+++ script.js
@@ -13,24 +13,45 @@ const markers = document.getElementById("markers");
 const html = document.getElementById("html");
 const body = document.getElementById("body");
 
-ipcRenderer.on("sendTheme", (event, data) => {
-  theme = data;
+ipcRenderer.on("sendTheme", (event, newData) => {
+  theme = newData;
   html.style.filter = (theme.dark) ? 'invert(0%)' : 'invert(100%)';
-  html.style.transform = (theme.flipped) ? 'rotate(-90deg)' : 'rotate(0deg)';
-  html.style.width = (theme.flipped) ? '100vh' : '100vw';
-  html.style.height = (theme.flipped) ? '100vw' : '100vh';
+  html.style.transform = (theme.flipped) ? 'rotateZ(-180deg)' : '';
+  /* html.style.width = (theme.flipped) ? '100vh' : '100vw';
+  html.style.height = (theme.flipped) ? '100vw' : '100vh'; */
 });
 
 ipcRenderer.on("sendControls", (event, newControls, newData) => {
-  controls = newControls;
+  controls = newControls
   data = newData
-  let steps = parseInt(data.width / controls.markers)
+
+  let stepSize = Math.ceil(data.width / controls.markers)
+  let markersNumber = []
+
+  if(controls.symmetrical){
+    let markersCount = Math.floor(controls.markers/2)
+    stepSize = Math.ceil((data.width / 2) / markersCount)
+    for (let i = markersCount; i > 0; i--){
+      markersNumber.push(i)
+    }
+    markersNumber.push(0)
+    for (let i = 1; i <= markersCount; i++){
+      markersNumber.push(i)
+    }
+  }else{
+    for (let i = 0; i < controls.markers; i++){
+      markersNumber.push(i)
+    }
+  }
+  
   markers.innerHTML = ''
-  for (let i = 0; i < controls.markers; i++) {
+  for (let i = 0; i < markersNumber.length; i++){
+    let step = markersNumber[i]
+    let spanText = (controls.showSizes) ? step + ':' + step*stepSize : step
     let div = document.createElement("div")
-    div.className = "marker"
     let span = document.createElement("span")
-    let text = span.append(i + ':' + i*steps)
+    let text = span.append(spanText)
+    div.className = "marker"
     div.append(span)
     markers.append(div)
   }
